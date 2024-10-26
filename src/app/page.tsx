c629d625946ec6717b7500de9604864e00510c38
Fn-front <reservoir-station@outlook.jp>
2024-10-26 22:04:18 +0900

    add: page chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { textDemo } from '@/functions/constants/demo'
+import ChatContent from '@/features/ui/chat/content'
+
 export default function Home() {
   return (
     <>
-      <h2 className="c_h2">テンプレ</h2>
+      <h2 className="c_h2">使用するテキストデータ</h2>
+      <p>{textDemo}</p>
+      <h2 className="c_h2">リンクのPDFと動画処理出力結果</h2>
+      {textDemo.map((item, index) => (
+        <div key={index}>
+          <ChatContent text={item} />
+        </div>
+      ))}
     </>
   );
 }
