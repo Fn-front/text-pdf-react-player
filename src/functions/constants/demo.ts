81420997a975582eb8383b5b7a89546aa19f82a2
Fn-front <reservoir-station@outlook.jp>
2024-10-26 22:57:26 +0900

    add: デモテキストにyoutubeリンク追加

--- src/functions/constants/demo.ts
+++ src/functions/constants/demo.ts
@@ -1,6 +1,6 @@
 export const textDemo = [
   '通常のURL: https://example.com',
   'PDFファイル：https://example.com/document.pdfをご確認ください。',
-  '動画ファイル：https://example.com/video.mp4をご覧ください。',
+  '動画ファイル：https://youtu.be/SyAO2288G3s?si=DueYSbDpMI9-GKJqをご覧ください。',
   '複数の種類: まずはこちらの資料（https://example.com/guide.pdf）を確認し、その後で動画（https://example.com/tutorial.mp4）をご覧ください。',
 ];
