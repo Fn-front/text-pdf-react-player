049d84b65e892832b943dc796239714b1583516a
Fn-front <reservoir-station@outlook.jp>
2024-11-10 15:42:19 +0900

    update: urlをenv管理に変更

--- src/functions/constants/demo.ts
+++ src/functions/constants/demo.ts
@@ -1,6 +1,8 @@
+const url = process.env.NEXT_PUBLIC_URL;
+
 export const textDemo = [
   '通常のURL: https://example.com',
   'PDFファイル：https://example.com/document.pdfをご確認ください。',
   '動画ファイル：https://youtu.be/SyAO2288G3s?si=DueYSbDpMI9-GKJqをご覧ください。',
-  '複数の種類: まずはこちらの資料（https://example.com/guide.pdf）を確認し、その後で動画（https://example.com/tutorial.mp4）をご覧ください。',
+  `複数の種類: まずはこちらの資料（https://example.com/guide.pdf）を確認し、その後で動画（${url}example.mov）をご覧ください。`,
 ];
